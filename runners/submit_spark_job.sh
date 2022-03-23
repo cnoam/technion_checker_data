1259b5f094e0462fb780d602b2d4558f5fbf2932
Noam Cohen <noam1023@gmail.com>
2022-03-23 10:14:40 +0200

    Update script to submit a job to SPARK
    Warning: contains sensi tive info

--- runners/submit_spark_job.sh
+++ runners/submit_spark_job.sh
@@ -1,27 +1,34 @@
 #!/bin/bash -eux
 
+# for unknown reason, if the azcopy returns with a failure when run from the command line, it hangs when runs in the script
+# and don't even trip the timeout.
+# I added "> junk" redirect and for one time it did work and the call returned. However it is not working properly now.
+
 LIVY_PASS="%Qq12345678"
 SRC_FILE=$1
 MY_SERVER=jobs.eastus.cloudapp.azure.com
-#test setup
-#CLUSTER_NAME=noam-c3
-#STORAGE_NAME=noamc3hdistorage
-#CONTAINER_NAME=noam-c3-2021-04-06t10-05-57-099z
+AZ_COPY_TIMEOUT_SEC=5
 
-#SECRET_SIG="sp=racwl&st=2021-06-24T05:00:23Z&se=2021-09-01T13:28:23Z&spr=https&sv=2020-02-10&sr=c&sig=4IIHWei9gAY4LqkZd3qN7v%2B%2BqU8JWHHMzAJDCpokAJ0%3D"
-#SECRET_SIG="sp=racwl&st=2021-08-10T09:21:43Z&se=2022-03-01T19:21:43Z&sv=2020-08-04&sr=c&sig=h2X%2BvHhAtbF%2BnXvCdfKzcNx7hGWW%2FDCvczyR4pBA30w%3D"
+TEST_MODE=0
 
-# ---------------------
-#production setup
-CLUSTER_NAME=noam-spark
-STORAGE_NAME=noamcluster1hdistorage
-CONTAINER_NAME=ex2
-# Create the SAS using the GUI in the portal, by going to the storage,
-# in "Data storage" select "Containers.
-# select the container (e.g. "ex2" ).
-# on the right hand side there is "..." and insdie "generate SAS"
-SECRET_SIG="sp=rw&st=2021-07-01T12:00:01Z&se=2025-01-01T21:40:51Z&spr=https&sv=2020-02-10&sr=c&sig=nCydcOygfUbu2Z66BPsjeIkF54kLSArJi0H%2BXXhL54w%3D"
-
+if [ $TEST_MODE -eq 1 ]; then
+   echo ---- test setup -----
+   CLUSTER_NAME=noam-c3
+   STORAGE_NAME=noamc3hdistorage
+   CONTAINER_NAME=noam-c3-2021-04-06t10-05-57-099z
+   SECRET_SIG="sp=racwl&st=2021-06-24T05:00:23Z&se=2021-09-01T13:28:23Z&spr=https&sv=2020-02-10&sr=c&sig=4IIHWei9gAY4LqkZd3qN7v%2B%2BqU8JWHHMzAJDCpokAJ0%3D"
+else
+   #production setup
+   CLUSTER_NAME=spark96224
+   STORAGE_NAME=noamcluster1hdistorage
+   CONTAINER_NAME=ex2
+   # Create the SAS using the GUI in the portal, by going to the storage,
+   # in "Data storage" select "Containers.
+   # select the container (e.g. "ex2" ).
+   # on the right hand side there is "..." and insdie "generate SAS"
+   # key for noam1hdstorage
+   SECRET_SIG="sp=racw&st=2022-03-22T14:06:27Z&se=2022-06-30T21:06:27Z&sv=2020-08-04&sr=c&sig=IrLmcnjxV9TJWPOl4Da0%2F%2FdMbXzoC0eGMgQsL6cYAYk%3D"
+fi
 
 # for the uploaded file (to the azure storage) we keep only the relative name to avoid directory naming problems
 REL_PATH_SRC_FILE=`echo $SRC_FILE | cut -d'/' -f 4`
@@ -30,10 +37,13 @@ REL_PATH_SRC_FILE=`echo $SRC_FILE | cut -d'/' -f 4`
 echo Uploading source file $SRC_FILE
 export AZCOPY_LOG_LOCATION="/logs"
 export AZCOPY_JOB_PLAN_LOCATION="/logs"
-PATH=$PATH:$CHECKER_DATA_DIR/runners  # so azcopy etc. is in the path
+PATH=$CHECKER_DATA_DIR/runners:$PATH  # so azcopy etc. is in the path
 echo PATH==$PATH
-azcopy copy $SRC_FILE "https://$STORAGE_NAME.blob.core.windows.net/$CONTAINER_NAME/$REL_PATH_SRC_FILE?$SECRET_SIG" 
-
+timeout $AZ_COPY_TIMEOUT_SEC `azcopy copy $SRC_FILE "https://$STORAGE_NAME.blob.core.windows.net/$CONTAINER_NAME/$REL_PATH_SRC_FILE?$SECRET_SIG"  > junk_azcopy`
+# bad syntax below
+#[ $? -ne 0 ]; then
+#   echo azcopy timed out or just failed. Is the server properly configured? Run the azcopy command from a terminal and check the output
+#fi
 
 echo Sending source for execution
 # send to spark for processing
