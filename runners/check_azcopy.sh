a591293f2ad303be98d7f6310b625027d4e59d36
Noam Cohen <cnoam@technion.ac.il>
2022-08-24 13:34:29 +0300

    update secret in check_azcopy

--- runners/check_azcopy.sh
+++ runners/check_azcopy.sh
@@ -4,14 +4,15 @@ touch empty_file
 SRC_FILE="empty_file"
 
 #production setup
-CLUSTER_NAME=noam-spark
-STORAGE_NAME=noamcluster1hdistorage
+#CLUSTER_NAME=noam-spark
+#STORAGE_NAME=noamcluster1hdistorage
 CONTAINER_NAME=ex2
 # Create the SAS using the GUI in the portal, by going to the storage,
 # in "Data storage" select "Containers.
 # select the container (e.g. "ex2" ).
 # on the right hand side there is "..." and insdie "generate SAS"
-SECRET_SIG="sp=rw&st=2021-07-01T12:00:01Z&se=2025-01-01T21:40:51Z&spr=https&sv=2020-02-10&sr=c&sig=nCydcOygfUbu2Z66BPsjeIkF54kLSArJi0H%2BXXhL54w%3D"
+# the secret has this format
+#SECRET_SIG="sp=rw&st=2021-07-01T12:00:01Z&se=2025-01-01T21:40:51Z&spr=https&sv=2020-02-10&sr=c&sig=nCydcOygfUbu2Z66BPsjeIkF54kLSArJi0H%2BXXhL54w%3D"
 
 export AZCOPY_LOG_LOCATION="/logs"
 export AZCOPY_JOB_PLAN_LOCATION="/logs"
